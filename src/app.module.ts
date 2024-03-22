d1813654142c9cdafdca9cd2f9b29c4408b11927
Aaron Davis <aaron@ripemetrics.com>
2024-03-22 07:42:22 -0700

    Add clean script and remove TasksController from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksController } from './tasks/tasks.controller';
 
 @Module({
   imports: [TasksModule],
-  controllers: [TasksController],
+  controllers: [],
 })
 export class AppModule {}
