1ede5627197736b4554e60b982554e135707708a
Aaron Davis <aaron@ripemetrics.com>
2024-03-25 16:53:03 -0700

    creating tasks, DTO

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getALlTasks(): Task[] {
+  // Get all tasks
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
+  @Post()
+  // Create a new task
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
 }
+//   createTask(
+//     // The @Body() decorator is used to extract the title and description from the request body
+//     @Body('title') title: string,
+//     @Body('description') description: string,
+//     // set the return type of the createTask method to Task
+//   ): Task {
+//     // call the createTask method from the tasksService and pass in the title and description
+//     return this.tasksService.createTask(title, description);
+//   }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
+  //Return all tasks
   getAllTasks(): Task[] {
+    //
     return this.tasks;
+  }
+
+  //Create a new task and add it to the tasks array
+  // takes in a title and description and returns the created task
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      // generate a unique id for the task
+      id: uuid(),
+      // set the title and description of the task and
+      title,
+      description,
+      // sets the status of the task to OPEN
+      status: TaskStatus.OPEN,
+    };
+
+    // add the created task to the tasks array
+    this.tasks.push(task);
+    return task;
   }
 }
